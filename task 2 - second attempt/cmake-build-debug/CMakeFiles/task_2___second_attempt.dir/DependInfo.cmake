5b7586b5f5175d6ac1ea2c263800ec58c74b3b66
Dasha <dasha.mok02@gmail.com>
2020-10-14 21:03:41 +0300

    Ta-da))) That is the finished lab 4. I'm terribly sorry that it took so long for me to finish this task. I hope you don't mind

---
+++ task 2 - second attempt/cmake-build-debug/CMakeFiles/task_2___second_attempt.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "G:/labs/lab 4/task 2 - second attempt/task 2 - 4 - attempt 2.cpp" "G:/labs/lab 4/task 2 - second attempt/cmake-build-debug/CMakeFiles/task_2___second_attempt.dir/task_2_-_4_-_attempt_2.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
