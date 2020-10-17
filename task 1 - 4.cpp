67c1e65b2cf03819ba329e61520ddad7fab3f834
Dasha <dasha.mok02@gmail.com>
2020-10-17 19:27:28 +0300

    task 1 - all done

--- task 1 - 4.cpp
+++ task 1 - 4.cpp
@@ -11,15 +11,32 @@ string comparing_of_strings(char* &str1, char* &str2) {
     while (str2[n2] != '\0') {
         n2 = ++n2;
     }
-    if (n1 > n2) {
-        return "first";
+    int compare = 0;
+    for(int i = 0, j = 0; ((str1[i] != '\0') && (str2[j] != '\0')); ++i, ++j) {
+    if (str1[i] == str2[j]){
+        compare = 0;
+        continue;
     }
-    if (n1 < n2) {
-        return "string";
+    if (str1[i] > str2[j]){
+        compare = 1;
+        break;
     }
-    if (n1 == n2) {
-        return "equal";
+    if (str1[i] < str2[j]){
+        compare = -1;
+        break;
     }
+}
+    if ((n1 > n2) && (compare == 0)) {
+        compare = 1;
+    }
+    if ((n1 < n2) && (compare == 0)) {
+        compare = -1;
+    }
+switch (compare) {
+    case 0: return "equal";
+    case 1: return "first";
+    case -1: return "second";
+}
 }
 
 int main() {
@@ -38,5 +55,7 @@ int main() {
     if (comparing_of_strings(str1, str2) == "equal") {
         cout << "These strings are equal.";
     }
+    delete [] str1;
+    delete [] str2;
     return 0;
 }
