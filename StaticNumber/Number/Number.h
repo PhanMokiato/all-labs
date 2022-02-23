aec949c3a1b1e9dd8f90b295ef0f6640a4fca3f1
Dasha <dasha.mok02@gmail.com>
2022-02-23 22:31:12 +0300

    os lab1

---
+++ StaticNumber/Number/Number.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace MyNumber {
+    class Number {
+    private:
+        double _num;
+    public:
+        Number(const double &num) : _num(num) {}
+        Number operator+(Number num1);
+        Number operator-(Number num1);
+        Number operator*(Number num1);
+        Number operator/(Number num1);
+        double getValue();
+    };
+    const Number one(1.0);
+    const Number zero(0.0);
+    inline Number createNumber(double num) {
+        return Number(num);
+    }
+}
